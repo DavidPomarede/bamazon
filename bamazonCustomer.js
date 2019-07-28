b968dd1b03a935df6ce2c86ff380a38264153574
doadet <davidpom@gmail.com>
2019-07-28 17:18:22 -0400

    added price total at the end

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,30 +1,10 @@
-
-// Then create a Node application called bamazonCustomer.js. Running this application will first display 
-// all of the items available for sale. Include the ids, names, and prices of products for sale.
-
-// The app should then prompt users with two messages.
-
-// The first should ask them the ID of the product they would like to buy.
-// The second message should ask how many units of the product they would like to buy.
-
-// Once the customer has placed the order, your application should check if your store has enough of 
-// the product to meet the customer's request.
-
-// If not, the app should log a phrase like Insufficient quantity!, and then prevent the order from going through.
-
-// However, if your store does have enough of the product, you should fulfill the customer's order.
-
-// This means updating the SQL database to reflect the remaining quantity.
-// Once the update goes through, show the customer the total cost of their purchase.
-
 var inquirer = require('inquirer');
 var mysql = require("mysql");
 
 
 var requestedID;
+var requestedItemPrice;
 var requestedQuantity;
-
-
 
 
 var connection = mysql.createConnection({
@@ -45,7 +25,7 @@ function afterConnection() {
     connection.query("SELECT * FROM products", function(err, res) {
       if (err) throw err;
       for (var i = 0; i < res.length; i++) {
-        console.log(res[i].id + " | " + res[i].product_name + " | " + res[i].price);
+        console.log(res[i].id + " | " + res[i].product_name + " | $" + res[i].price);
       }
       console.log("-----------------------------------");
       askCustomer();
@@ -56,73 +36,80 @@ function afterConnection() {
 
 function askCustomer() {
 
-  inquirer
-  .prompt([
-    {
-      type: "input",
-      message: "Please enter the ID number of the product you would like to buy: ",
-      name: "productID",
-    //   validate: function(value) {
-    //     if (isNaN(value) === false) {
-    //       return true;
-    //     }
-    //     return false;
-    //   }
-    },
-  ])
-  .then(function(answer1) {
-    requestedID = answer1.productID;
-    howManyUnits();
-
-});
-
+    inquirer
+    .prompt([
+            {
+            type: "input",
+            message: "Please enter the ID number of the product you would like to buy: ",
+            name: "productID",
+            validate: function(value) {
+                if (isNaN(value) === false) {
+                    return true;
+                }
+                return false;
+                }
+            },
+    ])
+    .then(function(answer1) {
+        requestedID = answer1.productID;
+        connection.query("SELECT * FROM products WHERE ?", { id: requestedID }, function(err, res) {
+            if (err) throw err;
+            for (var i = 0; i < res.length; i++) {
+                requestedItemPrice = res[i].price;
+            };
+        });
+        howManyUnits();
+    });
 };
 
 
 function howManyUnits() {
     inquirer
     .prompt([
-      {
-        type: "input",
-        message: "How many units would you like to buy?",
-        name: "unit_number",
-        // validate: function(value) {
-        //     if (isNaN(value) === false) {
-        //       return true;
-        //     }
-        //     return false;
-        //   }
-      },
+        {
+            type: "input",
+            message: "How many units would you like to buy?",
+            name: "unit_number",
+            validate: function(value) {
+                if (isNaN(value) === false) {
+                    return true;
+                }
+                return false;
+            }
+        },
     ])
     .then(function(answer2) {
-        console.log(answer2.unit_number);
+        requestedQuantity = answer2.unit_number;
         connection.query("SELECT * FROM products WHERE ?", { id: requestedID }, function(err, res) {
             if (err) throw err;
+
             for (var i = 0; i < res.length; i++) {
+                var newQuantity = res[i].stock_quantity - requestedQuantity;
                 if (answer2.unit_number < res[i].stock_quantity) {
-                    console.log("you will purchase one item");
+                    connection.query(
+                        "UPDATE products SET ? WHERE ?",
+                        [
+                            {
+                            stock_quantity: newQuantity
+                            },
+                            {
+                            id: requestedID
+                            }
+                        ],
+                        function(error) {
+                            // if (error) throw err;
+                            var total = requestedItemPrice * requestedQuantity;
+                            console.log("You purchased " + requestedQuantity + " of these items!");
+                            console.log("For a Total of $" + total + ".");
+                            connection.end();
+                        }
+                    );
                 } else {
-                    console.log("Insufficient Quantity!");
+                    console.log("Insufficient Quantity of this item!");
                     connection.end();
-                }
+                };
             }
         });
 
     });
 };
-
-
-
-
-// function checkHowMany(answer) {
-//     var query = "SELECT position, song, year FROM top5000 WHERE ?";
-//     connection.query(query, { id: answer.productID }, function(err, res) {
-//       if (err) throw err;
-//       for (var i = 0; i < res.length; i++) {
-//         console.log("Product: " + res[i].product_name+" Quantity: " + res[i].stock_quantity);
-//       }
-
-//       connection.end();
-//     });
-
-// };
